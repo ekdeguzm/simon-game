fcee65db7f6bb475bb99cd0cd077fd00f90fc689
Emil De Guzman <em.deguzm@gmail.com>
2024-05-21 15:55:52 -0700

    random color generator

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+
 function nextSequence() {
-  console.log(Math.floor(Math.random() * 4));
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  console.log(randomChosenColor);
 }
